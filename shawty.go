ebb41d1436c673bf02325a90e7d7f9f2b01ae503
Kyle Hardgrave <kyle@kylehardgrave.com>
2013-08-13 03:10:40 -0700

    WIP

--- shawty.go
+++ shawty.go
@@ -27,72 +27,71 @@ import (
 )
 
 func genShortcode(n int) string {
-	const ALPHABET =
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-"
+	const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-"
 	const BASE = 64
 
 	n += int(math.Pow(BASE, 3))
 	code := make([]string, 0)
 	for n > 0 {
-        r := n % BASE
-        n = n / BASE
+		r := n % BASE
+		n = n / BASE
 		code = append(code, string(ALPHABET[r]))
-        fmt.Printf("r=%d, n=%d, code=%s\n", r, n, code)
+		fmt.Printf("r=%d, n=%d, code=%s\n", r, n, code)
 	}
-    for i, j := 0, len(code)-1; i < j; i, j = i+1, j-1 {
-    	code[i], code[j] = code[j], code[i]
+	for i, j := 0, len(code)-1; i < j; i, j = i+1, j-1 {
+		code[i], code[j] = code[j], code[i]
 	}
 	return strings.Join(code, "")
 }
 
-func main() {
-    fmt.Println(genShortcode(0))
-}
+// func main() {
+//     fmt.Println(genShortcode(0))
+// }
 
 // Shorten the given URL and return the shortcode
-func ShortenUrl(url string) (code string, err error) {
-	var code string
-
-	for exists := true; exists; {
-		// Get the newest unique integer
-		n, err := redis.Int(conn.Do("INCR", URL_COUNT_KEY))
-		if err != nil {
-			return
-		}
-
-		// Generate its shortcode and make sure its not taken
-		code, err := genShortcode(n)
-
-		exists, err := redis.Bool(conn.DO("EXISTS", codeKey(code)))
-		if err != nil {
-			return
-		}
-	}
-
-	return
-}
-
-// Shorten the given URL to the given code. Return true if the URL was
-// successfully saved to the code, false if the code was taken.
-func ShortenUrlToCode(url string, code string) (success bool, err error) {
-	success := false
-
-	// Attempt to set the shortcode
-	wasSet, err := redis.Bool(conn.Do("SETNX", codeKey(code), url))
-	if err != nil {
-		return
-	}
-
-	if !wasSet {
-		setUrl, err := conn.Do("GET", codeKey(code))
-		success = setUrl == url
-		return
-	}
-
-	return true, nil
-}
-
-// Return the URL for the given shortcode.
-func LookupUrl(code sting) (url string, err error) {
-	return redis.String(conn.Do("GET", codeKey(code)))
-}
+// func ShortenUrl(url string) (code string, err error) {
+// 	var code string
+// 
+// 	for exists := true; exists; {
+// 		// Get the newest unique integer
+// 		n, err := redis.Int(conn.Do("INCR", URL_COUNT_KEY))
+// 		if err != nil {
+// 			return
+// 		}
+// 
+// 		// Generate its shortcode and make sure its not taken
+// 		code, err := genShortcode(n)
+// 
+// 		exists, err := redis.Bool(conn.DO("EXISTS", codeKey(code)))
+// 		if err != nil {
+// 			return
+// 		}
+// 	}
+// 
+// 	return
+// }
+// 
+// // Shorten the given URL to the given code. Return true if the URL was
+// // successfully saved to the code, false if the code was taken.
+// func ShortenUrlToCode(url string, code string) (success bool, err error) {
+// 	success := false
+// 
+// 	// Attempt to set the shortcode
+// 	wasSet, err := redis.Bool(conn.Do("SETNX", codeKey(code), url))
+// 	if err != nil {
+// 		return
+// 	}
+// 
+// 	if !wasSet {
+// 		setUrl, err := conn.Do("GET", codeKey(code))
+// 		success = setUrl == url
+// 		return
+// 	}
+// 
+// 	return true, nil
+// }
+// 
+// // Return the URL for the given shortcode.
+// func LookupUrl(code sting) (url string, err error) {
+// 	return redis.String(conn.Do("GET", codeKey(code)))
+// }
