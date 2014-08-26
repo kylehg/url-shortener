91fec3de2f331f96434b73848048258d8e424c4c
Kyle Hardgrave <kyle@kylehardgrave.com>
2014-08-26 13:21:24 -0700

    More wip, wrote tests

--- shawty/data.go
+++ shawty/data.go
@@ -5,23 +5,27 @@ import "github.com/garyburd/redigo/redis"
 const (
 	NETWORK = "tcp"
 	ADDRESS = ":6379"
+	DEFAULT_DB = 0
+	TEST_DB = 2
 	REDIS_KEY_PREFIX = "shawty:"
 	REDIS_URL_KEY_PREFIX = REDIS_KEY_PREFIX + "url:"
 	REDIS_SHORTCODE_KEY_PREFIX = REDIS_KEY_PREFIX + "code:"
 )
 
-var conn
+var redisConn
 
 // Get the Redis connection
 func getConn() redis.Conn {
 	var err
-	if conn != nil {
-		conn, err = redis.Dial(NETWORK, ADDRESS)
-		if err != nil {
+	if redisConn != nil {
+		if redisConn, err = redis.Dial(NETWORK, ADDRESS); err != nil {
 			panic("Error connecting to Redis database: " + err.Error())
 		}
+	  if _, err = conn.Do("SELECT", DEFAULT_DB); err != nil {
+	    panic("Cannot select default database")
+	  }
 	}
-	return conn
+	return redisConn
 }
 
 // Convert a shortcode to a shotcode key for Redis
@@ -34,38 +38,89 @@ func urlKey(url string) string {
 	return REDIS_URL_KEY_PREFIX + url
 }
 
+func redisGet(key string) (string, error) {
+	return redis.String(getConn().Do("GET", key)
+}
+
+func redisSetIfNotExists(key string, val string) (bool, error) {
+	return redis.Bool(getConn().Do("SET", key, val, "NX"))
+}
+
 // Lookup the URL for a given shortcode
-func GetUrlFromCode(code string) (string, error) {
-	return redis.String(getConn().Do("GET", codeKey(code)))
+func GetUrl(code string) (string, error) {
+	return redisGet(codeKey(code))
 }
 
-// Lookup the shortcode for a given URL
-func GetCodeFromUrl(url string) (string, error) {
-	return redis.String(getConn().Do("GET", urlKey(url)))
+// Lookup the default (random) shortcode for a given URL
+func GetDefaultCode(url string) (string, error) {
+	return redisGet(urlKey(url))
 }
 
-// Set the URL for a given shortcode it the code doesn't already exist
-func SetUrlForCode(code string, url string) error {
-	conn := getConn()
-	existingUrl, err := GetUrlFromCode(code)
-	if url == existingUrl {
-		return nil
-	}
-	if err != redis.ErrNil {
-		return // TODO return error "Code %s already maps to URL %s"
-	}
-
-	wasSet, err := redis.Bool(conn.Do("SETNX", codeKey(code), url))
+// Sets a custom, nondefault shortcode for a given URL
+func SetCustomCode(url string, code string) error {
+	wasSet, err := redisSetIfNotExists(codeKey(code), url)
 	if err != nil {
 		return err
 	}
 
-	urlMapped, err := redis.Bool(conn.Do("SETNX", urlKey(url), code))
-	if !urlMapped || err != nil {
-		return // TODO return error "URL %salready mapped by code %s" (may be race condition)
+	if !wasSet {
+		existingUrl, err := GetUrl(code)
+		if err != nil {
+			return err
+		} else if existingUrl == url {
+			return nil
+		}
+		return // TODO ERROR code is already taken
 	}
 
 	return nil
+}
+
+// Set the defua for a given shortcode it the code doesn't already exist
+func SetDefaultCode(url string, code string) error {
+	conn := getConn()
+
+	conn.Send("MULTI")
+	conn.Send("SET", codeKey(code), url, "NX")
+	conn.Send("SET", urlKey(url), code, "NX")
+	responses, err := conn.DO("EXEC")
+	if err != nil {
+		return err
+	}
+
+
+
+	// Check if default code is used
+	// If used and maps to same url, return nil
+	// If used and maps to different url, return error
+	existingUrl, err := GetUrl(code)
+	if err != redis.ErrNil {
+		if existingUrl == url {
+			return nil
+		}
+		return err
+	}
+
+	// Set code -> url
+	// If fails, return error
+	wasSet, err := redisSetIfNotExists(codeKey(code), url)
+	if !wasSet {
+		return // TODO Race error
+	} else if err != nil {
+		return err
+	}
+
+	// Set url -> code
+	// If fails:
+	// - unset code -> url
+	// - return error
+	wasSet, err := redisSetIfNotExists(urlKey(url), code)
+	if !wasSet || if err != nil {
+		// T
+		return err
+	}
+
+	// Else return nil
 }
 
 // Get a random, unused shortcode for a URL
---
+++ shawty/data_test.go
@@ -0,0 +1,83 @@
+package shawty
+
+import (
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"testing"
+)
+
+func TestGetAndSet(t *testing.T) {
+	var err, code, url
+
+	google := "https://www.google.com/"
+	facebook := "https://www.facebook.com/"
+	medium := "https://medium.com/"
+
+	conn := getConn()
+	if _, err = conn.Do("SELECT", TEST_DB); err != nil {
+		panic("Cannot select test database")
+	}
+	conn.Do("FLUSHDB")
+
+	if err = SetDefaultCode(google, "googl"); err != nil {
+		t.Error("Setting a default code should work: " + err.Error())
+	}
+
+	if url, err = GetUrl("googl"); url != google || err != nil {
+		t.Error("Getting by a default code should work: " + err.Error())
+	}
+
+	if code, err = GetDefaultCode(google); code != "googl" || err != nil {
+		t.Error("Getting a default code by URL should work: " + err.Error())
+	}
+
+	if err = SetCustomCode(facebook, "f"); err != nil {
+		t.Error("Setting a custom code should work: " + err.Error())
+	}
+
+	if url, err = GetUrl("f"); url != facebook || err != nil {
+		t.Error("Getting by a custom code should work: " + err.Error())
+	}
+
+	if code, err = GetDefaultCode(facebook); code != "" || err == nil {
+		t.Error("Getting a default code when there's only a custom code should return nil")
+	}
+
+	if err = SetDefaultCode(google, "moogl"); err == nil {
+		t.Error("Setting multiple default codes should fail")
+	}
+
+	if err = SetCustomCode(facebook, "fb"); err != nil {
+		t.Error("Setting multiple custom codes should work: " + err.Error())
+	}
+
+	if err = SetDefaultCode(facebook, "faceb"); err != nil {
+		t.Error("Setting a default code and a custom code should work: " + err.Error())
+	}
+
+	if err = SetDefaultCode(medium, "googl"); err == nil {
+		t.Error("Overwriting a default code should fail")
+	}
+
+	if url, err = GetUrl("googl"); url != google || err != nil {
+		t.Error("The original default code should not be overwritten: " + err.Error())
+	}
+
+	if err = SetCustomCode(medium, "f"); err == nil {
+		t.Error("Overwriting a custom code should fail")
+	}
+
+	if url, err = GetUrl("f"); url != facebook || err != nil {
+		t.Error("The original custom code should not be overwritten: " + err.Error())
+	}
+
+	if err = SetDefaultCode(google, "googl"); err != nil {
+		t.Error("Resetting the same default code should succeed: " + err.Error())
+	}
+
+	if err = SetCustomCode(facebook, "fb"); err != nil {
+		t.Error("Resetting the same custom code should succeed: " + err.Error())
+	}
+
+	conn.Do("FLUSHDB")
+}
